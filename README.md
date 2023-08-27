1ac6e621f7268e715e061b145abed85ba6d13508
JoniclerSaldanha01 <jonicler.isaias.saldanha@gmail.com>
2023-08-27 12:18:31 -0300

    Debug e Pause

--- README.md
+++ README.md
@@ -10,3 +10,10 @@
  
 * [Status do Projeto]() :[Em Desenvolvimento] 
    
+- [x] Debug <br>
+![Alt text](image-3.png)
+![Alt text](image-4.png)
+- [x] Pause <br>
+![Alt text](image-5.png)
+![Alt text](image-6.png)
+![Alt text](image-8.png)
--- cypress/e2e/basic.spec.cy.js
+++ cypress/e2e/basic.spec.cy.js
@@ -2,13 +2,14 @@
 
 
 describe('Cypress basics', () => {
-    it('Should visit a page and assert title', () => {
+    it.only('Should visit a page and assert title', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         // const title = cy.title()
         // console.log(title)
 
         // VALIDA O NOME DA PÁGINA
+        cy.pause()
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
             .and('contain', 'Campo')
